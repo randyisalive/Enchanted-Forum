53b38c296c3110ce3fa56d572a8542106cf05bea
randyisalive <99945506+randyisalive@users.noreply.github.com>
2022-04-13 12:27:31 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 # save this as app.py
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
 
--- db.py
+++ db.py
@@ -1,1 +1,11 @@
 import pymysql
+
+def db_connection():
+    conn = pymysql.connect(
+        host='localhost',
+        port=3306,
+        database='flask_forum',
+        user='root',
+        password=''
+    )
+    return conn
